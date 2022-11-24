7b84bd7c05cd19ca0853eee843455e77df1b592b
Jhon Useche <jhon.useche@telus.com>
2022-11-23 21:30:18 -0700

    Implementation of vertical and horizontal wordserach. Assignment completed during pair programming assignment with @RickCarr

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,9 +1,46 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
+
+  it("should return 'This Array is Empty!'", function() {
+    const result = wordSearch([], 'EMPTY');
+    assert.strictEqual(result, 'This Array is Empty!');
+  });
+
+  it("should return 'Enter a word to search.'", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'J', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'H', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'O', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'N', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'U', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+    assert.strictEqual(result, 'Enter a word to search.');
+  });
+
+  it("should return true if the word is vertical", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'J', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'H', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'O', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'N', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'U', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'JHONU');
+
+    assert.isTrue(result);
+  });
+
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -15,12 +52,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is horizontal", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,7 +68,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (word.length === 0) return 'Enter a word to search.';
+  if (letters.length === 0) return 'This Array is Empty!';
+  
+  //horizontal
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  
+  for (const line of horizontalJoin) {
+    if (line.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  // vertical
+  const transposedArray = [];
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      transposedArray.push([]);
+    }
+    break;
+  }
+
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      transposedArray[j].push(letters[i][j]);
+    }
+  }
+
+  const verticalJoin = transposedArray.map(ls => ls.join(''));
+  
+  for (const line of verticalJoin) {
+    if (line.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
